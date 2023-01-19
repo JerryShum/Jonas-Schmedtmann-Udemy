9c3c38e39a28f9f99d3c31b605a4d1ec1e2a04eb
Jerry Shum <30613366+JurryShrooms@users.noreply.github.com>
2023-01-18 19:11:02 -0500

    Update script.js

--- JS - Jonas Schmedtmann/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ JS - Jonas Schmedtmann/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//! Exercises https://gist.github.com/akozdev/9653dc7cbd811f9447d6bb6f3f9ac4f8
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';

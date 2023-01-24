3ac2a6d62df48613d2e21c87ebd36e12e7d42707
Jerry Shum <30613366+JurryShrooms@users.noreply.github.com>
2023-01-23 19:38:33 -0500

    update

---
+++ JS - Jonas Schmedtmann/complete-javascript-course-master/09-Data-Structures-Operators/starter/Exercises/destructuringObjects.js
@@ -0,0 +1,98 @@
+/* Data used in exercises */
+const books = [
+    {
+        title: 'The Lord of the Rings',
+        publicationDate: '1954-07-29',
+        author: 'J. R. R. Tolkien',
+        genres: ['fantasy', 'high-fantasy', 'adventure'],
+        filmAdaptation: true,
+        otherLanguagesTitle: {
+            spanish: 'El señor de los anillos',
+            chinese: '魔戒',
+            french: 'Le Seigneur des anneaux'
+        }
+    },
+    {
+        title: 'The Cyberiad',
+        publicationDate: 1965,
+        author: 'Stanislaw Lem',
+        genres: ['science fiction'],
+        otherLanguagesTitle: {
+            polish: 'Cyberiada',
+            spanish: 'Ciberiada',
+            french: 'Cybériade'
+        }
+    },
+    {
+        title: 'Dune',
+        publicationDate: 1965,
+        author: 'Frank Herbert',
+        genres: ['science fiction', 'novel', 'adventure'],
+        filmAdaptation: true,
+        otherLanguagesTitle: {}
+    },
+    {
+        title: 'Harry Potter and the Philosopher\'s Stone',
+        publicationDate: '1997-06-26',
+        author: 'J. K. Rowling',
+        genres: ['fantasy', 'adventure'],
+        filmAdaptation: true,
+        otherLanguagesTitle: {
+            spanish: 'Harry Potter y la piedra filosofal',
+            korean: '해리 포터와 마법사의 돌',
+            bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
+            portuguese: 'Harry Potter e a Pedra Filosofal'
+        }
+    },
+    {
+        title: 'A Game of Thrones',
+        publicationDate: '1996-08-01',
+        author: 'George R. R. Martin',
+        genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
+        filmAdaptation: true,
+        otherLanguagesTitle: {
+            korean: '왕좌의 게임',
+            polish: 'Gra o tron',
+            portuguese: 'A Guerra dos Tronos',
+            spanish: 'Juego de tronos'
+        }
+    }
+];
+
+/* ⚠️ YOU WILL CALL THE FUNCTIONS BELOW IN EXERCISES.
+     DON'T WORRY IF THEY DON'T MAKE SENSE FOR NOW.
+     YOU WILL LEARN EVERYTHING A BIT LATER IN THE COURSE.
+     FOR NOW TREAT THEM AS BLACK BOXES (focus on the values they return).
+     YOU CAN CALL THEM AND LOG THE RETURNED VALUE TO THE CONSOLE TO SEE WHAT EXACTLY THEY RETURN. */
+
+const getBooksByGenre = genre => books.filter(book => book.genres.includes(genre));
+const getBooksAsArrays = () => books.map(book => Object.entries(book));
+const getBookAuthors = () => books.map(book => book.author);
+
+/*
+*  ********************************************
+*  2) DESTRUCTURING OBJECTS                   *
+*  ********************************************
+*/
+
+/* A) Take the first object from the 'books' array, and assign the author to the 'author' variable using destructuring.
+      Use the 'let' statement because the 'author' variable may change later. */
+let { author } = books[0];
+console.log(author); //!J. R. R. Tolkien
+
+
+/* B) Take the second object from the 'books' array, and destructure the title into a variable called 'bookTitle'. */
+const { title: bookTitle } = books[1];
+console.log(bookTitle); //! The Cyberiad
+
+/* C) The book objects aren't consistent in their form.
+      For example, the second book doesn't have the 'filmAdaptation' property.
+      Destructure it into a variable called 'hasFilmAdaptation' with a default value of false. */
+
+
+/* D) Remember the 'author' variable from exercise A? It's time to reassign it.
+      Destructure the author of the third book into existing variable called 'author'. */
+
+
+
+/* ⚠️ COMMENT OUT YOUR SOLUTIONS AFTER YOU FINISH SO THAT IT DOESN'T COLLIDE WITH NEXT EXERCISES 🠕 */
--- JS - Jonas Schmedtmann/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ JS - Jonas Schmedtmann/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+//? Destructuring Arrays
+/*
+//> A way of unpacking values from an array or an object into separate variables
+//Breaking a larger data structure into a smaller one like a variable
+
+//________________________________________________________________________________________________________________
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -27,18 +38,6 @@ const restaurant = {
     },
   },
 };
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-
-//? Destructuring Arrays
-/*
-//> A way of unpacking values from an array or an object into separate variables
-//Breaking a larger data structure into a smaller one like a variable
-
-//________________________________________________________________________________________________________________
 
 //! An example of destructuring
 //We are trying to take the elements within the arr array and convert them into a standalone variable
@@ -98,3 +97,94 @@ console.log(p, q, r); //r is 1
 */
 
 //? Destructuring Objects
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time, address }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`);
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//> In order to dsestructure objects, we use {}, Example using the restaurant object:
+//! Define the properties that you want to take -> this will store them into a variable with the same name
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//! If we want variable names to be different than property:
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//> Default Values
+//! It might be useful to set DEFAULT values incase we dont get a value from the object (value instead of undefined)
+//We set default values using an EQUAL SIGN, in this case, since menu doesn't exist as a property inside of restaurant, it will return an empty array instead of undefined. since starterMenu exists, its name will be starters and NOT return an empty array
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//> Mutating variables from objects
+//We want to change the OG a and b variables to the ones inside of the object (111, 999) -> (23, 7)
+let a = 111;
+let b = 999;
+const randObj = { a: 23, b: 7, c: 14 };
+
+//! Must wrap this thing inside of parantheses b/c "{}" means that JS engine expects a code block
+({ a, b } = randObj);
+console.log(a, b);
+
+//> Nested Objects (restaurant -> opening hours -> days)
+//Inside opening hours -> friday object -> inside friday object -> open and close properties -> turn open and close into variables
+const { fri: { open, close } } = openingHours;
+console.log(open, close);
+
+//> Practical Application Example:
+//Define orderDelivery function inside of the restaurant object
+// REMEMBER WE ARE ONLY PASSING IN AN OBJECT INTO THE FUNCTION (NOT 4 ARGUMENTS)
+// Basically we are defining the object that is THE ARGUMENT of this function -> and then we can destructure that object right inside of the argument -> accessing and using the properties within said object
+
+//! Since we destructured the object directly, the function can now access the variables of said object
+//It will now output delivery details
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+})
+
+//!We can also set default values if some arguments/variables/properties cannot be destructured
+//Since this object inputted into the function doesn't have mainIndex and starterIndex -> default values will be used
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+})
+
+
+
+

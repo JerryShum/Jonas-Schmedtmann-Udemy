265c444ce8b0575f05a4c31b00c350034a69791e
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2023-08-05 12:50:41 -0400

    Update script.js

--- JS - Jonas Schmedtmann/complete-javascript-course-master/14-OOP/starter/script.js
+++ JS - Jonas Schmedtmann/complete-javascript-course-master/14-OOP/starter/script.js
@@ -103,7 +103,7 @@ console.dir(x => x + 1); //FUNCTIONS are also a prototype
 
 
 //? ES6 Classes
-
+/*
 //! Class Expression
 // const PersonCL = class{};
 
@@ -157,7 +157,7 @@ jerma.greet();
 //? Setters and Getters
 // basically functions that get and set a value
 // acts as properties aswell
-
+/*
 const account = {
     owner: 'Jonas',
     movements: [200, 53, 120, 300],
@@ -193,9 +193,9 @@ console.log(account.movements);
 // we get an error if we set have this.fullname (we replace it with _fullname) (basically a loop since we keep going into the fullname method)
 // to fix the issue of not having a fullName property (since it's _fullname now ) -> we use a GETTER method/property
 
-const walter = new PersonCl('Walter White', 1950);
-console.log(walter.fullName);
-
+// const walter = new PersonCl('Walter White', 1950);
+// console.log(walter.fullName);
+*/
 
 /////////////////////////////////////////////////////
 
@@ -205,15 +205,15 @@ console.log(walter.fullName);
 //* manually set the prototype of an object to any other object that we want
 
 // creating an object to be the prototype of all the person objects
-
+/*
 const PersonProto = {
     calcAge() {
-        console.log(2037 - birthYear);
+        console.log(2037 - this.birthYear);
     },
 
     init(firstName, birthYear) {
         this.firstName = firstName;
-        this.birthyear = birthYear;
+        this.birthYear = birthYear;
     },
 
 };
@@ -230,5 +230,158 @@ console.log(steven.__proto__); //Equal to PersonProto
 const sara = Object.create(PersonProto);
 sara.init('Sara', 1979);
 sara.calcAge();
+*/
+
+/////////////////////////////////////////////////////
 
 
+//? Inheritance Between "Classes": Using Constructor Functions 
+
+/*
+const PersonConstructor = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+PersonConstructor.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+    // this.firstName = firstName;
+    // this.birthYear = birthYear;
+
+    //* To prevent dupe code:
+    // doesn't work without the .call method b/c we are actually calling the Person() function as a normal function call (without the new operator (which is used when creating a new object) -> the this. keyword is set to undefined in normal function calls -> doesn't work properly)
+    // .call method allows an object to use a method from another object -> Student is using the Person function -> .call can take in an argument (in this case, it is using the this. keyword which is referring to the object of which it is called) -> this. = student
+    // meaning student will fill in the this.firstname and this.birthyear properties
+    PersonConstructor.call(this, firstName, birthYear);
+
+    this.course = course;
+}
+
+//! Assigning the Student.prototype object to inherit from the Person.prototype object
+// The student.prototype object is the prototype of all STUDENT OBJECTS (mike.__proto__ = Student.prototype)
+// Now we are saying that the prototype of the student.prototype object is the Person.prototype object (student.prototype.__proto__ = Person.prototype)
+// we need this b/c we want it to INHERIT from the person.prototype
+
+
+Student.prototype = Object.create(PersonConstructor.prototype);
+
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+
+// since the Student.prototype inherits from the Person.prototype object, we can use their associated methods on a STUDENT OBJECT (prototype chain)
+mike.calcAge();
+
+
+//! We need to fix this:
+// this is an issue since it thinks that the constructor of Student objects is the Person Constructor
+console.dir(Student.prototype.constructor);
+
+//* To fix this:
+// we are basically manually assigning the constructor
+Student.prototype.constructor = Student;
+
+//ALL TRUE since mike is an object of the prototype chain
+console.log(mike instanceof Student); //true
+console.log(mike instanceof Person); //true
+console.log(mike instanceof Object); //true
+*/
+
+
+/////////////////////////////////////////////////////
+
+
+//? Inheritance Between "Classes": Using ES6 Classes
+
+/*
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    };
+
+    //Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    set fullName(name) {
+        console.log(name);
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} does not include space`)
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+}
+
+//! Extends = "is a" -> student is a person
+class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+
+        //* Always needs to happen first
+        //Super is basically the constructor of the PARENT class
+        super(fullName, birthYear);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+
+    //! Overriding a method from the parent class (polymorphism)
+    calcAge() {
+        console.log(`I'm ${2037 - this.birthYear} years old, but as student I feel more like ${2037 - this.birthYear + 10}`);
+    }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science')
+
+martha.introduce();
+martha.calcAge();
+*/
+
+
+/////////////////////////////////////////////////////
+
+
+//? Inheritance Between "Classes": Using Object.create
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const steven = Object.create(PersonProto);
+
+//* Making PersonProto the prototype of StudentProto (StudentProto will inherit from PersonProto)
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+
+    //! We use this to prevent repeat code
+    // we use the call. method so that the this. as a parameter since we want the this. keyword in PersonProto.init to refer to the StudentProto object
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+// jay who is a student now inherits from StudentProto -> jay will inherit from both StudentProto and PersonProto
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();

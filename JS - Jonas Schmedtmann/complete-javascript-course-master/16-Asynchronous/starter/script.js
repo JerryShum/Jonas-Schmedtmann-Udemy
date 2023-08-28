ca8106610b5d408b5889d056f428bc70f4b5283b
JerryShum <jerryshum2636@gmail.com>
2023-08-28 15:47:33 -0400

    Update script.js

--- JS - Jonas Schmedtmann/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ JS - Jonas Schmedtmann/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -170,23 +170,51 @@ const getCountryData = function (country) {
 
 //? Handling Rejected Promises / Catching errors
 // What we do when the promise is REJECTED/NOT FULFILLED (fetch request is disconnected or somehting) -> UNCAUGHT PROMISE
+// .catch()
+
+//? Handling/Throwing errors manually
+// Manually throwing specific error codes when .catch isn't catching the errors
+// throw new error
 
 const renderError = function (message) {
     countriesContainer.insertAdjacentText('beforeend', message);
     countriesContainer.style.opacity = 1;
 };
+/*
 
+
+//* Making a helper function
+const getJSON = function (url, errorMsg = 'Something went wrong.') {
+    return fetch(url).then(response => {
+        if (!response.ok) {
+            throw new Error(`${errorMsg} (${response.status})`)
+        }
+
+        return response.json();
+    })
+}
+*/
 
 //! Simplified code:
+// without helper:
+/*
 const getCountryData = function (country) {
     // Fetch returns a promise -> .then is used on promises when they are FULFILLED -> allows a callback function
     //! Country 1
     fetch(`https://restcountries.com/v3.1/name/${country}`)
         // First callback function -> fullfilled || Second -> error/rejected
-        .then(
-            response => response.json()
+        .then(response => {
+            console.log(response + 'yes');
+
+            // If the ok property of the response object is false (not false = true) -> throw an error message -> rejects the entire promise for the entire chain -> propagates down to the .catch method -> err = the error we throw -> changes the error message 
+            if (!response.ok) {
+                throw new Error(`Country not found (${response.status})`)
+            }
+
+            //Manually return since it's not the only line in the arrow function
+            return response.json()
             //,err => alert(err)
-        )
+        })
         .then(data => {
             console.log(data);
 
@@ -225,8 +253,102 @@ const getCountryData = function (country) {
         .finally(() => countriesContainer.style.opacity = 1)
         ;
 };
+*/
+
+/*
+// With helper function
+const getCountryData = function (country) {
+    // Fetch returns a promise -> .then is used on promises when they are FULFILLED -> allows a callback function
+    //! Country 1
+    getJSON(`https://restcountries.com/v3.1/name/${country}`, `Country not found`)
+        .then(data => {
+            console.log(data);
+
+            renderCountry(data[0]);
+
+            // Once again, using optional chaining -> data[0] exists (api) -> .borders (accessing borders property) -> ?. (optional chaining checks whether the .borders property exists) -> returns undefined if not, and accesses borders[0] if it does exist
+            const neighbour = data[0].borders?.[0];
+
+            console.log(neighbour);
+
+            //! IF THERE IS NO NEIGHBOUR
+            if (!neighbour) throw new Error('No neighbour found');
+
+            return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, 'country not found');
+
+        })
+        .then(data => renderCountry(data[0], 'neighbour'))
+        //! using a .catch method to handle all errors in one place
+        .catch(err => {
+            console.error(`${err} ERROR DETECTED`);
+            renderError(`Something went wrong ${err}`);
+        })
+        .finally(() => countriesContainer.style.opacity = 1)
+        ;
+};
 
 btn.addEventListener('click', function () {
-    getCountryData('canada');
+    getCountryData('australia');
 });
+*/
+
+//? Consuming Promises with Async/Await
+// async functions will run in the background while performing the code inside of it, RETURNS A PROMISE afterwards
+// inside the function, you have AWAIT -> needs a promise (can use the promise returned from the fetch function) -> stops the code execution until it is fulfilled 
+
+//? Error Handling with try...catch
+// if an error were to  occur in the code inside of the try block, the catch block will be able to catch it
+// IMPLEMENT THIS IN THE CODE INSIDE ASYNC FUNCTIONS
+// -> also manually catching errors after each fetch just in case
+
+// try {
+//     let y = 1;
+//     const x = 2;
+//     y = 3;
+// } catch(error){
+//     alert(error.message)
+// }
+
+//! Geolocation with promises
+const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(
+            position => resolve(position),
+            err => reject(err)
+        );
+    });
+};
+
+//* Essentially the same as fetch.then -> just a cleaner shroud
+
+const whereAmI = async function (country) {
+    try {
+        //! geolocation: 
+        const pos = await getPosition();
+        const { latitude: lat, longitude: lng } = pos.coords;
+
+        //  Reverse geocoding
+        const responseGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+        const dataGeo = await responseGeo.json();
+        console.log(dataGeo);
+        if (!responseGeo.ok) throw new Error('Problem getting location data')
+
+
+        //! Countries:
+        const response = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
+        if (!response.ok) throw new Error('Problem getting country')
+
+        //* Get json out of the response
+        // response.json() returns ANOTHER RESPONSE -> we use another await keyword to retrieve it
+        const data = await response.json();
+        console.log(data);
+        renderCountry(data[0]);
+    } catch (error) {
+        console.log(error);
+        renderError(`something went wrong ${error.message}`)
+    }
+}
+
+whereAmI('canada')
+
 
